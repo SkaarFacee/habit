4c9cd31722698b567f298c380d3be1d360bdf982
Aditya Anil <aditya8anil@gmail.com>
2025-07-05 13:18:04 +1000

    Feat: Optimized the code for saving the json

--- utils.py
+++ utils.py
@@ -52,30 +52,60 @@ class SaveUtils:
             for future in as_completed(futures):
                 tracker_entries.append(future.result())
         
-
-        for entry in tracker_entries:
-            for list_name in entry.keys():
-                if list_name not in self.data['Tracker'].keys():
-                    ('List not found in tracker')
-                    self.data['Tracker'].update(entry)
-                else: 
-                    for date_key in entry[list_name].keys():
-                        if not date_key in self.data['Tracker'][list_name].keys():
-                            self.data['Tracker'][list_name][date_key]=[task_info]
-                        else:
-                            for task_info in entry[list_name][date_key]:
-                                if task_info not in self.data['Tracker'][list_name][date_key]:
-                                    self.data['Tracker'][list_name][date_key].append(task_info)
-        grouped_by_date = {}
-
+        self.create_json(tracker_entries)
+        
         for list_name in self.data['lists']:
-            for date_key in self.data['Tracker'][list_name].keys():
-                for tasks in self.data['Tracker'][list_name][date_key]:
-                    for x in response[list_name]:
-                        if x['completed'] == 'Not complete' :
-                                if x['title']==tasks['title']:
-                                    del tasks
+            tracker = self.data['Tracker'][list_name]
+            for date_key in list(tracker.keys()):
+                tasks = tracker[date_key]
+                not_complete_titles = {
+                    x['title']
+                    for x in response.get(list_name, [])
+                    if x['completed'] == 'Not complete'
+                }
+                tracker[date_key] = [
+                    task for task in tasks if task['title'] not in not_complete_titles
+                ]
+                if not tracker[date_key]:
+                    del tracker[date_key]
         self.save_json()
+
+
+
+    def create_json(self, tracker_entries):
+        tracker = self.data['Tracker']
+        import json 
+        json.dump(tracker_entries,open('tracker_entries.json','w'))
+        for entry in tracker_entries:
+            for list_name, dates in entry.items():
+                list_data = tracker.setdefault(list_name, {})
+                for date_key, tasks in dates.items():
+                    date_data = list_data.setdefault(date_key, [])
+                    titles_in_date=[x['title'] for x in date_data]
+                    for task in tasks:
+                        if task['title'] not in titles_in_date:
+                            date_data.append(task)
+
+
+
+    # BETA VERSION 
+    # def create_json(self,tracker_entries):
+    #     for entry in tracker_entries:
+    #         for list_name in entry.keys():
+    #             if list_name not in self.data['Tracker'].keys():
+    #                 self.data['Tracker'].update(entry)
+    #                 continue 
+    #             else: 
+    #                 for date_key in entry[list_name].keys():
+    #                     if not date_key in self.data['Tracker'][list_name].keys():
+    #                         self.data['Tracker'][list_name][date_key]=entry[list_name][date_key]
+    #                         continue
+    #                     else:
+    #                         for task_info in entry[list_name][date_key]:
+    #                             if task_info not in self.data['Tracker'][list_name][date_key]:
+    #                                 self.data['Tracker'][list_name][date_key].append(task_info)
+
+
 
 
     def save_json(self):
